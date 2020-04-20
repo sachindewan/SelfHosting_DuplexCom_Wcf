9373768c72a8b117eed409254a3e3fb950d7ccc1
Sachin Kumar <sachin.kumar06@nagarro.com>
2020-04-20 14:59:40 +0530

    Implemented Instance management using service behavior

--- SampleWcfService/Process.cs
+++ SampleWcfService/Process.cs
@@ -8,6 +8,14 @@ using System.ServiceModel;
 namespace SampleWcfService
 {
     [ServiceBehavior(ConcurrencyMode =ConcurrencyMode.Reentrant)]
+
+    //insatance management
+    //Per cal/ even if same user has been logged or even for same session
+    //[ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall)]
+    //Per session/per logged in user
+    //[ServiceBehavior(InstanceContextMode = InstanceContextMode.PerSession)]
+    //Single for every  context
+    //[ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
     public class Process : IProcess
     {
         public void TaskProcess()
